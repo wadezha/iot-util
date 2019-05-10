15983492d6be3ada99b45c33f4156b9c62d77418
wadecha <339028973@qq.com>
2019-05-10 18:00:05 +0800

    1. id generator fix 2. mysql_pool change file name mysql_client 3. redis_client file: add setex function, fix other function

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 require('./lib/buffer');
 
 const geography = require('./lib/geography');
-const idGenerator = new (require('./lib/id_generator'))();
+const IdGenerator = require('./lib/id_generator');
 const Logger = require('./lib/logger');
 const EventBus = require('./lib/event_bus');
-const MysqlPool = require('./lib/mysql_pool');
+const MysqlClient = require('./lib/mysql_client');
 const HttpClient = require('./lib/http_client');
 const RedisClient = require('./lib/redis_client');
 const PyramidBlock = require('./lib/pyramid_block');
 const MessageCenter = require('./lib/message_center');
 
 exports.geography = geography;
-exports.idGenerator = idGenerator;
+exports.IdGenerator = IdGenerator;
 exports.Logger = Logger;
 exports.EventBus = EventBus;
-exports.MysqlPool = MysqlPool;
+exports.MysqlClient = MysqlClient;
 exports.HttpClient = HttpClient;
 exports.RedisClient = RedisClient;
 exports.PyramidBlock = PyramidBlock;
